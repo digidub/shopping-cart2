c403eb92b512403b547718b97dd7d07bc46fe3f7
Sholto <madein.mania@gmail.com>
2021-06-08 15:27:56 +0100

    styling of components

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -11,14 +11,14 @@ const ProductCard = (props) => {
       <div className='product-image'>
         <Image src={image} alt={altText} />
       </div>
-      <div className='product-info'>
+      <ProductInfo>
         <div className='product-name'>{name}</div>
         <div className='product-price' ref={priceRef}>
           £{price}
         </div>
-      </div>
+      </ProductInfo>
       <input type='number' min='1' defaultValue={1} ref={itemRef} />
-      <input type='submit' value='Add to Cart' />
+      <AddToCart type='submit' value='Add to Cart' />
     </Card>
   );
 };
@@ -27,7 +27,7 @@ export default ProductCard;
 
 const Card = styled.div`
   flex: 1 16%;
-  padding: 2%;
+  margin: 2%;
   display: flex;
   flex-direction: column;
 `;
@@ -35,4 +35,17 @@ const Card = styled.div`
 const Image = styled.img`
   max-width: 100%;
   height: 200px;
+`;
+
+const ProductInfo = styled.div`
+  margin-top: auto;
+`;
+
+const AddToCart = styled.input`
+  background: white;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
 `;
