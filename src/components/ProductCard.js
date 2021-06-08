7a5e1c9c262d000ee8487725cd33b6dc2179dcaa
Sholto <madein.mania@gmail.com>
2021-06-08 15:06:25 +0100

    added DOM elements to return function for rending product details

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
-const ProductCard = () => {
-    return (    );
-}
- 
+const ProductCard = (props) => {
+  const itemRef = useRef();
+  const priceRef = useRef();
+
+  return (
+    <div className='product-card'>
+      <div className='product-image'>
+        <img src={props.image} alt={props.altText} />
+      </div>
+      <div className='product-info'>
+        <div className='product-name'>{props.name}</div>
+        <div className='product-price' ref={priceRef}>
+          £{props.price}
+        </div>
+      </div>
+      <input type='number' min='1' defaultValue={1} ref={itemRef} />
+      <input type='submit' value='Add to Cart' />
+    </div>
+  );
+};
+
 export default ProductCard;
