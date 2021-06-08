f8a1a416d8985065480e02ee3bfbe47e9dc93991
Sholto <madein.mania@gmail.com>
2021-06-08 15:07:04 +0100

    destructured prop values

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,16 +3,17 @@ import React, { useRef } from 'react';
 const ProductCard = (props) => {
   const itemRef = useRef();
   const priceRef = useRef();
+  const { image, altText, name, price } = props;
 
   return (
     <div className='product-card'>
       <div className='product-image'>
-        <img src={props.image} alt={props.altText} />
+        <img src={image} alt={altText} />
       </div>
       <div className='product-info'>
-        <div className='product-name'>{props.name}</div>
+        <div className='product-name'>{name}</div>
         <div className='product-price' ref={priceRef}>
-          £{props.price}
+          £{price}
         </div>
       </div>
       <input type='number' min='1' defaultValue={1} ref={itemRef} />
