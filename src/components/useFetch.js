9cf9ba64e2d32d727544f5873674385b04d12375
Sholto <madein.mania@gmail.com>
2021-06-08 14:58:50 +0100

    added basic state object

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,6 +1,13 @@
 import { useEffect, useState } from 'react';
 
 const useFetch = () => {
+    const [state, setState] = useState({
+        loading: true,
+        error: false,
+        data: [],
+      });
+
+
     return (  );
 }
  
