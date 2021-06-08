56f5bb1f8a05ca42bf17a5a694ca0df0205739f6
Sholto <madein.mania@gmail.com>
2021-06-08 14:41:22 +0100

    basic navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (  );
-}
- 
+  return (
+    <ul>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/'>Products</Link>
+      </li>
+      <li>
+        <Link to='/'>View Cart</Link>
+      </li>
+    </ul>
+  );
+};
+
 export default Navbar;
