7ed78c88253ef9dbe2f3c0d97314fc81bed74dfd
Sholto <madein.mania@gmail.com>
2021-06-08 15:00:02 +0100

    useEffect mount to fetch API data and resolve promise

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -1,14 +1,27 @@
 import { useEffect, useState } from 'react';
 
 const useFetch = () => {
-    const [state, setState] = useState({
-        loading: true,
-        error: false,
-        data: [],
-      });
+  const [state, setState] = useState({
+    loading: true,
+    error: false,
+    data: [],
+  });
 
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setState({ loading: false, error: false, data });
+      })
+      .catch((error) => setState({ loading: false, error, data: [] }));
+  }, []);
 
-    return (  );
-}
- 
+  return state;
+};
+
 export default useFetch;
