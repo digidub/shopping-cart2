85e2e19795273f3165ed9adbfc25543c54a129be
Sholto <madein.mania@gmail.com>
2021-06-09 14:59:51 +0100

    fixed cart context import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { Fragment, useReducer } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { checkIfItemInCart, deleteItemLookup, removeQuantityFromBasket } from '../../shopping-cart/src/components/AppLogic';
-import { CartContext } from '../../shopping-cart/src/components/CartContext';
 import Cart from './components/Cart';
+import { CartContext } from './components/CartContext';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import ProductPage from './components/ProductPage';
