6062e2b479b4851a5a803ad511c4dc2ee1a8f238
Sholto <madein.mania@gmail.com>
2021-06-09 09:44:44 +0100

    initiating context and reducer for managing API data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment, useContext, useReducer } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Cart from './components/Cart';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Products from './components/Products';
 
 function App() {
+  const dataContext = useContext();
+  const [data, dispatch] = useReducer({
+    loading: true,
+    error: false,
+    data: [],
+  });
+
   return (
     <Fragment>
       <BrowserRouter>
@@ -15,6 +23,9 @@ function App() {
           </Route>
           <Route exact path='/products'>
             <Products />
+          </Route>
+          <Route exact path='/cart'>
+            <Cart />
           </Route>
         </Switch>
       </BrowserRouter>
