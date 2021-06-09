4036708ed20453a8509c74f2481e8e78b16ba4bc
Sholto <madein.mania@gmail.com>
2021-06-09 14:49:22 +0100

    reducer function for controlling state of cart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import { createContext, Fragment, useContext, useState } from 'react';
+import { Fragment, useReducer } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { checkIfItemInCart, deleteItemLookup, removeQuantityFromBasket } from '../../shopping-cart/src/components/AppLogic';
+import { CartContext } from '../../shopping-cart/src/components/CartContext';
 import Cart from './components/Cart';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import ProductPage from './components/ProductPage';
 import Products from './components/Products';
 
-export const StoreDataContext = createContext();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increment':
+      const cartItemsIncrement = checkIfItemInCart(action.item, state.items, 'add');
+      return {
+        ...state,
+        items: cartItemsIncrement,
+        itemCount: state.itemCount + action.quantity,
+        costCount: state.costCount + action.cost * action.quantity,
+      };
+    case 'decrement':
+      const cartItemsDecrement = checkIfItemInCart(action.item, state.items, 'subtract');
+      return {
+        ...state,
+        items: cartItemsDecrement,
+        itemCount: state.itemCount - action.quantity,
+        costCount: state.costCount - action.cost * action.quantity,
+      };
+    case 'delete':
+      const newCart = deleteItemLookup(action.item, state.items);
+      const itemsToBeSubtracted = removeQuantityFromBasket(action.item, state.items);
+      return {
+        ...state,
+        items: newCart,
+        itemCount: state.itemCount - itemsToBeSubtracted,
+        costCount: state.costCount - action.cost * itemsToBeSubtracted,
+      };
+    default:
+      throw new Error('woops');
+  }
+};
 
 function App() {
-  const [storeData, setStoreData] = useState({
-    loading: true,
-    error: false,
-    data: [],
+  const [state, dispatch] = useReducer(reducer, {
+    items: [],
+    costCount: 0,
+    itemCount: 0,
   });
 
   return (
     <Fragment>
       <BrowserRouter>
-        <StoreDataContext.Provider value={{ storeData, setStoreData }}>
+        <CartContext.Provider value={{ state, dispatch }}>
           <Navbar />
           <Switch>
             <Route exact path='/'>
@@ -34,7 +66,7 @@ function App() {
               <ProductPage />
             </Route>
           </Switch>
-        </StoreDataContext.Provider>
+        </CartContext.Provider>
       </BrowserRouter>
     </Fragment>
   );
