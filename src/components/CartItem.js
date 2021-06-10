23273f09c9263598db36bfb0cf21818d2be38a0b
Sholto <madein.mania@gmail.com>
2021-06-10 09:25:40 +0100

    styling of components

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,14 +1,13 @@
 import React, { useContext } from 'react';
 import { CartContext } from './CartContext';
+import styled from 'styled-components';
 
 const CartItem = (props) => {
   const { dispatch } = useContext(CartContext);
 
   return (
-    <div className='cart-item'>
-      <div className='cart-item-img'>
-        <img src={props.image} alt='changeme' />
-      </div>
+    <CartCard>
+      <CartImage src={props.image} alt='changeme' />
       <div className='cart-item-name'>
         <p>{props.name}</p>
       </div>
@@ -67,8 +66,23 @@ const CartItem = (props) => {
           delete
         </button>
       </div>
-    </div>
+    </CartCard>
   );
 };
 
 export default CartItem;
+
+const CartCard = styled.div`
+  display: grid;
+  grid-template-columns: 10% 30% 5% 55%;
+  height: 10vh;
+`;
+
+const ImageHolder = styled.div`
+  height: 8vh;
+`;
+
+const CartImage = styled.img`
+  max-height: 100%;
+  overflow: hidden;
+`;
