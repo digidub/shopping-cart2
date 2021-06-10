a2ca7426e47deceacea8021ee95b713f9a402172
Sholto <madein.mania@gmail.com>
2021-06-10 08:53:49 +0100

    style cart display

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,16 @@
 import React, { useContext } from 'react';
 import { CartContext } from './CartContext';
+import CartItem from './CartItem';
+import styled from 'styled-components';
 
 const Cart = () => {
   const { state } = useContext(CartContext);
 
   const cartItems = state.items.map((product) => {
     return (
-      <CartItem
-        key={product.id}
-        id={product.id}
-        image={product.image}
-        name={product.name}
-        price={product.price}
-        quantity={product.quantity}
-      ></CartItem>
+      <CartDisplay>
+        <CartItem key={product.id} id={product.id} image={product.image} name={product.name} price={product.price} quantity={product.quantity} />
+      </CartDisplay>
     );
   });
 
@@ -21,3 +18,8 @@ const Cart = () => {
 };
 
 export default Cart;
+
+const CartDisplay = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
