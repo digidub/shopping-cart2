7e273d941ada70351dd5fb1c7310bf09012f41ce
Sholto <madein.mania@gmail.com>
2021-06-10 11:33:41 +0100

    nav bar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import CartNavDisplay from './CartNavDisplay';
 
 const Navbar = () => {
   return (
-    <ul>
-      <li>
-        <NavLink to='/'>Home</NavLink>
-      </li>
-      <li>
-        <NavLink to='/products'>Products</NavLink>
-      </li>
-      <li>
-        <NavLink to='/cart'>
-          <CartNavDisplay />
-        </NavLink>
-      </li>
-    </ul>
+    <NavBar>
+      <NavLinks>
+        <li>
+          <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='/products'>Products</NavLink>
+        </li>
+        <li>
+          <NavLink to='/cart'>
+            <CartNavDisplay />
+          </NavLink>
+        </li>
+      </NavLinks>
+    </NavBar>
   );
 };
 
 export default Navbar;
+
+const NavBar = styled.div`
+  background-color: grey;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 5vh;
+`;
+
+const NavLinks = styled.ul`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 600px;
+  margin: 0 auto;
+  list-style-type: none;
+`;
