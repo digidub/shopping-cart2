6f6bbf9304edd43d0ba915dc7b1a1bd741597c83
Sholto <madein.mania@gmail.com>
2021-06-10 08:48:06 +0100

    building out cart display

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from './CartContext';
 
 const Cart = () => {
-  return <p>cart</p>;
+  const { state } = useContext(CartContext);
+
+  const cartItems = state.items.map((product) => {
+    return (
+      <CartItem
+        key={product.id}
+        id={product.id}
+        image={product.image}
+        name={product.name}
+        price={product.price}
+        quantity={product.quantity}
+      ></CartItem>
+    );
+  });
+
+  return <div className='cart-container'>{cartItems}</div>;
 };
 
 export default Cart;
