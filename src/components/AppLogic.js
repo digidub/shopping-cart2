ee6bfd67c5908754b722e1767cf56299e78624da
Sholto <madein.mania@gmail.com>
2021-06-11 09:34:04 +0100

    refactoring reducer utilities to shallow copy state before editing it

--- src/components/AppLogic.js
+++ src/components/AppLogic.js
@@ -1,16 +1,34 @@
 export const checkIfItemInCart = (item, array, sum) => {
-  if (array.find((object) => object.id === item.id)) {
-    const toUpdate = array.find((object) => object.id === item.id);
-    if (sum === 'add') toUpdate.quantity = toUpdate.quantity + item.quantity;
-    else {
-      console.log('hi');
-      toUpdate.quantity = toUpdate.quantity - item.quantity;
-      if (toUpdate.quantity === 0) array = deleteItemLookup(item, array);
+  console.log(item);
+  const index = array.find((object) => object.id === item.id);
+  if (index !== undefined) {
+    const newArray = array.map((item, i) =>
+      i === index
+        ? {
+            ...item,
+            quantity: item.quantity + (sum === 'add' ? 1 : -1),
+          }
+        : item
+    );
+    if (!newArray[index]?.quantity) {
+      return deleteItemLookup(item, newArray);
     }
-    return array;
+    return newArray;
   }
   return array.concat(item);
 };
+
+//   if (array.find((object) => object.id === item.id)) {
+//     const toUpdate = array.find((object) => object.id === item.id);
+//     if (sum === 'add') toUpdate.quantity = toUpdate.quantity + item.quantity;
+//     else {
+//       toUpdate.quantity = toUpdate.quantity - item.quantity;
+//       if (toUpdate.quantity === 0) array = deleteItemLookup(item, array);
+//     }
+//     return array;
+//   }
+//   return array.concat(item);
+// };
 
 export const deleteItemLookup = (item, array) => {
   const deleted = array.filter((object) => object.id !== item.id);
