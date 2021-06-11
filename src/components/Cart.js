e2a208f0f1ac3d1a0962c0f37c434df25611e29e
Sholto <madein.mania@gmail.com>
2021-06-11 08:49:26 +0100

    styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -33,8 +33,8 @@ const CartDisplay = styled.div`
   margin-top: 10vh;
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  justify-content: flex-start;
+  align-items: stretch;
   background-color: lightgrey;
 `;
 
