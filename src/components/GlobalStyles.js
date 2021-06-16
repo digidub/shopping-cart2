12744e4e0d6a336ce8138de0d38269fe9c495f8e
Sholto <madein.mania@gmail.com>
2021-06-16 21:30:24 +0100

    import google font

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Work+Sans');
+* {
+    font-family: 'Work Sans';
+    }
+
 body {
-    background-color: #fdfdfd
+    background-color: #fdfdfd;
 }`;
+
+export default GlobalStyle;
