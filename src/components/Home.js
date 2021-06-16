7cba4d5a55c660c5df87093e43fa17f7a7727547
Sholto <madein.mania@gmail.com>
2021-06-16 21:31:30 +0100

    styling components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Home = () => {
-  return <p>hi</p>;
+  return (
+    <HomePage>
+      <h1>General Store</h1>
+    </HomePage>
+  );
 };
 
 export default Home;
+
+const HomePage = styled.div`
+  margin: 0 auto;
+  margin-top: 70px;
+  width: 768px;
+`;
