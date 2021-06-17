5cb4319c99c49f290fd79d4f5a436d12640c05ef
Sholto <madein.mania@gmail.com>
2021-06-17 08:43:13 +0100

    conditional fetch follow on

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -6,16 +6,25 @@ const useFetch = (props) => {
     error: false,
     data: [],
   });
+  console.log(props);
 
   let limit = '';
-
-  if (props?.hasOwnProperty(limit)) {
-    limit = `?limit=${props.limit}`;
-  }
+  props?.length > 0 ? (limit = props) : (limit = '');
 
   useEffect(() => {
-    limit.length > 0
-      ? fetch(`https://fakestoreapi.com/products${limit}`)
+    limit?.length > 0
+      ? fetch(`https://fakestoreapi.com/products?limit=${limit}`)
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error(res.status);
+            }
+            return res.json();
+          })
+          .then((data) => {
+            console.log(data);
+            setState({ loading: false, error: false, data });
+          })
+          .catch((error) => setState({ loading: false, error, data: [] }))
       : fetch(`https://fakestoreapi.com/products`)
           .then((res) => {
             if (!res.ok) {
