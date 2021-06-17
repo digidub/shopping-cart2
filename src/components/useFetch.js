7fe6bbb03357f1734499f0ab542ab7bb874dffa7
Sholto <madein.mania@gmail.com>
2021-06-17 08:24:18 +0100

    conditional fetch based on whether api limit passed as prop

--- src/components/useFetch.js
+++ src/components/useFetch.js
@@ -7,19 +7,27 @@ const useFetch = (props) => {
     data: [],
   });
 
+  let limit = '';
+
+  if (props?.hasOwnProperty(limit)) {
+    limit = `?limit=${props.limit}`;
+  }
+
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products')
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error(res.status);
-        }
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setState({ loading: false, error: false, data });
-      })
-      .catch((error) => setState({ loading: false, error, data: [] }));
+    limit.length > 0
+      ? fetch(`https://fakestoreapi.com/products${limit}`)
+      : fetch(`https://fakestoreapi.com/products`)
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error(res.status);
+            }
+            return res.json();
+          })
+          .then((data) => {
+            console.log(data);
+            setState({ loading: false, error: false, data });
+          })
+          .catch((error) => setState({ loading: false, error, data: [] }));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
